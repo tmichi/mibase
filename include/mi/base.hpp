491845902035cf02c2480625a86eabd87f913171
Takashi Michikawa <michi@MichikawaTakashi-no-MacBook.local>
2015-07-03 20:50:49 +0900

    snapshot

---
+++ include/mi/base.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file base.hpp
+ * @author Takashi Michikawa <michikawa@acm.org>
+ */
+#include "Argument.hpp"
+#include "Attribute.hpp"
+#include "AttributeSet.hpp"
+#include "BooleanAttribute.hpp"
+#include "Clamp.hpp"
+#include "CommandTemplate.hpp"
+#include "DoubleNumericAttribute.hpp"
+#include "Exporter.hpp"
+#include "FileLister.hpp"
+#include "FileNameConverter.hpp"
+#include "Importer.hpp"
+#include "Limits.hpp"
+#include "NumericAttribute.hpp"
+#include "ParallelFor.hpp"
+#include "Parse.hpp"
+#include "PriorityQueue.hpp"
+#include "Progress.hpp"
+#include "StringAttribute.hpp"
+#include "SystemInfo.hpp"
+#include "Thread.hpp"
+#include "Timer.hpp"
+#include "Tokenizer.hpp"
+#include "TripleNumericAttribute.hpp"
+
+/**
+ * @mainpage
+ * mibase library provides basic classes for building CUI-based application quickly. 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
