db7a59eb56beb2be01fc2458e46936c2464056b2
Takashi Michikawa <michikawa@acm.org>
2016-04-19 12:59:03 +0900

    SystemInfo divided

---
+++ src/SystemInfoInternal.hpp
@@ -0,0 +1,47 @@
+#ifndef MI_INTERNAL_SYSTEM_INFO_INTERNAL 
+#define MI_INTERNAL_SYSTEM_INFO_INTERNAL 1
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <sstream>
+
+//Windows
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)// Win32 API
+#ifndef	OS_WINDOWS
+   #define	OS_WINDOWS 1
+#include "SystemInfoInternalWindows.hpp"
+#endif	//OS_WINDOWS
+
+//Cygwin
+#elif defined (__CYGWIN__)
+#ifndef OS_CYGWIN
+#define OS_CYGWIN 1
+#include "SystemInfoInternalCygwin.hpp"
+#endif//OS_CYGWIN 
+
+#else //UNIX
+#ifndef OS_UNIX
+#define OS_UNIX 1
+#include "SystemInfoInternalUnix.hpp"
+#endif//OS_UNIX
+#if defined (__APPLE__)
+#ifndef OS_MAC
+#define OS_MAC 1
+#endif//OS_MAC
+
+#elif defined(__linux__)
+#ifndef OS_LINUX
+#define OS_LINUX 1
+#endif//OS_LINUX
+#else
+#ifndef OS_UNKNOWN
+#define OS_UNKNOWN 1
+#include "SystemInfoInternalUnknown.hpp"
+#endif//OS_UNKNOWN
+#endif//if defined __APPLE__ __linux
+
+#endif//if defined win, cyg, unix
+
+#endif //MI_INTERNAL_SYSTEM_INFO_INTERNAL_HPP
---
+++ src/SystemInfoInternalCygwin.hpp
@@ -0,0 +1,53 @@
+#ifndef MI_SYSTEM_INFO_INTERNAL_CYGWIN_HPP
+#define MI_SYSTEM_INFO_INTERNAL_CYGWIN_HPP 1
+#ifdef OS_CYGWIN
+#include "SystemInfoInternal.hpp"
+#include <ctime>
+
+namespace mi
+{
+	namespace internal {
+		class SystemInfoInternal {
+		public:
+			SystemInfoInternal ( void ) {
+				;
+			}
+			~SystemInfoInternal ( void ) {
+				;
+			}
+			virtual std::string getCpuName ( void ) const {
+				return std::string("Good CPU");
+			}
+			virtual double getMemorySize( void ) const {
+				return 0;
+			}
+			virtual int getNumCores( void ) const {
+				return 1;
+			}
+			virtual std::string getDate( void ) const {
+				time_t rawtime = std::time ( NULL );
+				struct tm* timeinfo = localtime ( &rawtime );
+				char buffer [80];
+				strftime ( buffer, 80, "%F-%X", timeinfo );
+				std::string result ( buffer );
+				return result;
+			}
+			virtual double getPeakMemory( void ) const {
+				double peakMemory = 0;
+				struct rusage rusage;
+				getrusage ( RUSAGE_SELF, &rusage );                ///@todo The result somewhat strange on Mac.
+				peakMemory = static_cast<double> ( rusage.ru_maxrss );
+				return peakMemory;
+			}
+			
+			static SystemInfoInternal& getInstance(void) {
+				static SystemInfoInternal instance;
+				return instance;
+			}
+
+
+		};
+	}
+}
+#endif //OS_CYGWIN
+#endif //MI_SYSTEM_INFO_INTERNAL_CYGWIN_HPP
