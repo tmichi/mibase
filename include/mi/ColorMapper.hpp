de6723e23f648bbad5d554049a9db41555520daa
Takashi Michikawa <tmichi@me.com>
2016-04-27 18:42:02 +0900

    ColorMapper.cpp added

--- include/mi/ColorMapper.hpp
+++ include/mi/ColorMapper.hpp
@@ -1,8 +1,5 @@
 #ifndef MI_COLOR_MAPPER_HPP
 #define MI_COLOR_MAPPER_HPP 1
-#include <mi/Clamp.hpp>
-#include <cmath>
-#include <iostream>
 namespace mi
 {
         /**
@@ -12,39 +9,14 @@ namespace mi
         class ColorMapper
         {
         private:
-                const double _lower;
-                const double _upper;
+		class Impl;
+		Impl *_impl;
         public:
-                explicit ColorMapper ( const double lower = 0.0, const double upper = 1.0 ) : _lower( lower ), _upper( upper ) {
-                        if ( lower >= upper ) std::cerr<<"warning. lower value is larger than upper value. "<<std::endl;
-                        return;
-                }
-
-                void convert ( const double value, double& r,  double& g, double& b ) {
-                        const double v = ( value - this->_lower ) / ( this->_upper - this->_lower ) * 4;
-                        r = mi::clamp( v   - 2.0                , 0.0, 1.0 );
-                        g = mi::clamp( 2.0 - std::fabs( 2.0 - v ), 0.0, 1.0 );
-                        b = mi::clamp( 2.0 - v                  , 0.0, 1.0 );
-                        return;
-                }
-
-                void convert ( const double value, int& r,  int& g, int& b ) {
-                        double r0, g0, b0;
-                        this->convert( value, r0, g0, b0 );
-                        r = static_cast<int>( r0 * 255 ) ;
-                        g = static_cast<int>( g0 * 255 ) ;
-                        b = static_cast<int>( b0 * 255 ) ;
-                        return;
-                }
-
-                void convert ( const double value, unsigned char& r,  unsigned char& g, unsigned char& b ) {
-                        int r0, g0, b0;
-                        this->convert( value, r0, g0, b0 );
-                        r = static_cast<unsigned char>( r0 ) ;
-                        g = static_cast<unsigned char>( g0 ) ;
-                        b = static_cast<unsigned char>( b0 ) ;
-                        return;
-                }
+                explicit ColorMapper ( const double lower = 0.0, const double upper = 1.0 );
+		~ColorMapper( void ) ;
+                void convert ( const double value, double& r,  double& g, double& b );
+                void convert ( const double value, int& r,  int& g, int& b );
+                void convert ( const double value, unsigned char& r,  unsigned char& g, unsigned char& b );
         };
 }
 #endif
